e9d394daf4d21b75cc5b1cd2e6d19a85d6b95fbf
yaya <saterlekchi@npo-rit.ru>
2025-08-01 16:29:13 +0300

    new9

--- docs/static/script/filter_lang.js
+++ docs/static/script/filter_lang.js
@@ -8,7 +8,7 @@ function get_all_langs() {
     return langs
 }
 
-function get_chd_langs() {
+function get_ckd_langs() {
     element_langs = document.getElementById("filter-langs");
     element_langs = element_langs.querySelectorAll('input[type="checkbox"]')
     const langs = []
@@ -20,7 +20,7 @@ function get_chd_langs() {
     return langs
 }
 
-function get_not_chd_langs() {
+function get_not_ckd_langs() {
     element_langs = document.getElementById("filter-langs");
     element_langs = element_langs.querySelectorAll('input[type="checkbox"]')
     const langs = []
@@ -33,17 +33,25 @@ function get_not_chd_langs() {
 }
 
 function show_lang() {
-    langs = get_chd_langs()
+    langs = get_ckd_langs()
     for (let i = 0; i < langs.length; i++) {
         hide_show_by_atribut("lang", langs[i], "input-checkbox-" + langs[i], "flex");
     }
 }
 
 function hide_lang(langs) {
-    langs = get_not_chd_langs()
+    langs = get_not_ckd_langs()
     for (let i = 0; i < langs.length; i++) {
         hide_show_by_atribut("lang", langs[i], "input-checkbox-" + langs[i], "none");
     }
+}
+
+function deleteAllCookies() {
+    document.cookie.split(';').forEach(cookie => {
+        const eqPos = cookie.indexOf('=');
+        const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    });
 }
 
 function filter_lang_handler(lang) {
@@ -59,6 +67,35 @@ function filter_lang_handler(lang) {
         element_lang.checked = true;
     }
 
+    if (element_lang.checked) {
+        document.cookie = "lang_" + lang + "=true; path=/";
+    } else {
+        document.cookie = "lang_" + lang + "=false; path=/";
+    }
+
+    update_layout();
+}
+
+function filter_lang_cookies() {
+
+    var keyValuePairs = document.cookie.split(';');
+    for (var i = 0; i < keyValuePairs.length; i++) {
+        const name = keyValuePairs[i].substring(0, keyValuePairs[i].indexOf('=')).trim();
+        const value = keyValuePairs[i].substring(keyValuePairs[i].indexOf('=') + 1).trim();
+
+        console.log(name, value);
+
+        const lang = name.replace("lang_", "")
+        element_lang = document.getElementById("input-checkbox-" + lang);
+
+        if (element_lang) {
+            if (value == "true") {
+                element_lang.checked = true;
+            } else {
+                element_lang.checked = false;
+            }
+        }
+    }
     update_layout();
 }
 
@@ -70,13 +107,13 @@ function update_layout() {
 
     single_width_min = single_width_min * emInPixels;
     var sum_single_width_max = single_width_min * get_all_langs().length * 0.8
-    var sum_single_width_min = single_width_min * get_chd_langs().length
+    var sum_single_width_min = single_width_min * get_ckd_langs().length
 
     show_lang();
 
-    if(sum_single_width_max < page_width){
+    if (sum_single_width_max < page_width) {
         changeCSSStyle('#filter-langs', 'flex-direction', 'row');
-    }else{
+    } else {
         changeCSSStyle('#filter-langs', 'flex-direction', 'column');
     }
 
@@ -92,5 +129,6 @@ function update_layout() {
     hide_lang();
 }
 
+document.addEventListener("DOMContentLoaded", filter_lang_cookies);
 document.addEventListener('DOMContentLoaded', update_layout);
 window.addEventListener('resize', update_layout);
